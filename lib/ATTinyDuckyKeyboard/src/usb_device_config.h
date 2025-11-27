a2f5ad886c475e85e1850b2a54da3aaccef72d60
3mrgnc3 <stuwertpatt@gmail.com>
2025-11-27 21:50:00 +0100

    updated to use DigiKey defaults

--- lib/ATTinyDuckyKeyboard/src/usb_device_config.h
+++ lib/ATTinyDuckyKeyboard/src/usb_device_config.h
@@ -6,24 +6,24 @@
 #ifndef USB_DEVICE_CONFIG_H
 #define USB_DEVICE_CONFIG_H
 
-// VID: 0x046d
-#define USB_VID_LOW     0x6d
-#define USB_VID_HIGH    0x04
+// VID: 0x16c0
+#define USB_VID_LOW     0xc0
+#define USB_VID_HIGH    0x16
 
-// PID: 0xc31e
-#define USB_PID_LOW     0x1e
-#define USB_PID_HIGH    0xc3
+// PID: 0x27db
+#define USB_PID_LOW     0xdb
+#define USB_PID_HIGH    0x27
 
-// Manufacturer: Logitech
-#define USB_VENDOR_NAME     'L','o','g','i','t','e','c','h'
-#define USB_VENDOR_NAME_LEN 8
+// Manufacturer: DigiKey
+#define USB_VENDOR_NAME     'D','i','g','i','K','e','y'
+#define USB_VENDOR_NAME_LEN 7
 
-// Product: K120 Keyboard
-#define USB_DEVICE_NAME     'K','1','2','0',' ','K','e','y','b','o','a','r','d'
-#define USB_DEVICE_NAME_LEN 13
+// Product: ATTiny85 Ducky
+#define USB_DEVICE_NAME     'A','T','T','i','n','y','8','5',' ','D','u','c','k','y'
+#define USB_DEVICE_NAME_LEN 14
 
-// Serial: 00010001
-#define USB_SERIAL_NUMBER     '0','0','0','1','0','0','0','1'
-#define USB_SERIAL_NUMBER_LEN 8
+// Serial: 0001
+#define USB_SERIAL_NUMBER     '0','0','0','1'
+#define USB_SERIAL_NUMBER_LEN 4
 
 #endif // USB_DEVICE_CONFIG_H
